1da8e0f001a1dda0220d08e28984e7de24504caf
Genry <amveu_ctrimer@mail.ru>
2023-12-14 20:04:56 +0300

    1_Version_commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число отличное от 1, если хотите ввести массив самостоятельнно. ");
+int choise = Convert.ToInt32(Console.ReadLine());
+if (choise==1)
+{
+
+   string basic = "Hello, 2, world, :-)" ;
+   string[] basicArray = basic.Split(", ");
+   // string []endArray= new string[basicArray.Length];
+   int el=0;
+   
+   for (int d=0;d<basicArray.Length;d++)
+   {
+      if(basicArray[d].ToString().Length<=3)
+      {
+         el+=1;
+      }
+   }
+   string[]endArray= new string[el];
+   int index= 0;
+      for (int d=0;d<basicArray.Length;d++)
+   {
+      if(basicArray[d].ToString().Length<=3)
+      {
+         endArray[index]=basicArray[d];
+         index+=1;
+      }
+    
+   }
+   
+ Console.WriteLine($"Массив: [{String.Join(", ", endArray)}]");
+   
+}
+else
+
+
